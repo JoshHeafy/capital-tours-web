14a492061fa9c4e39e7d5ef1959dfd16c17c93a2
unknown <i2225401@continental.edu.pe>
2023-09-01 15:49:06 -0500

    Update: Se mejoro la seccion de Nosotros

--- app/nosotros/page.jsx
+++ app/nosotros/page.jsx
@@ -3,45 +3,41 @@ import Footer from "@/components/Footer";
 export default function page() {
   return (
     <>
-      <section className="nosotros">
-        <div className="container">
-          <h2 className="nosotros-heading">Acerca de Capital Tours</h2>
-          <p className="nosotros-description">
-            Capital Tours es una empresa líder en el sector de servicios de
-            suscripción de vehículos para taxi en Huancayo, Perú...
-          </p>
-
-          <h3 className="mission-heading">Nuestra Misión</h3>
-          <p className="mission-description">
-            Nuestra misión en Capital Tours es ofrecer a nuestros clientes
-            conductores de taxi una alternativa innovadora y rentable...
-          </p>
-
-          <h3 className="services-heading">Nuestros Servicios</h3>
-          <ul className="services-list">
-            <li>Suscripción de Vehículos para Taxi</li>
-            <li>Soporte y Mantenimiento</li>
-          </ul>
-
-          <h3 className="why-us-heading">Por qué elegirnos</h3>
-          <ul className="why-us-list">
-            <li>Experiencia y Confianza</li>
-            <li>Variedad de Vehículos</li>
-            <li>Proceso Sencillo</li>
-          </ul>
-
-          <h3 className="contact-heading">Contáctanos</h3>
-          <p className="contact-description">
-            Si estás interesado en nuestras suscripciones de vehículos para taxi
-            en Huancayo, no dudes en contactarnos...
-          </p>
-          <p className="contact-info">
-            Dirección: Av. Mariscal Castilla & Av. Gral. Cordova Huancayo, Perú
-            <br />
-            Teléfono: 914354014
-            <br />
-            Horario de Atención: 8:30 am - 7:00 pm{" "}
-          </p>
+      <section class="about-us">
+        <div class="about">
+          <div className="title">
+            <h2>Nosotros</h2>
+          </div>
+          <div className="content-about">
+            <img
+              src="/img/newcapital.png"
+              class="pic"
+              alt="imagen de la empresa"
+            />
+            <div class="text">
+              <h5>
+                NewCapital-<span>CapitalTours</span>
+              </h5>
+              <p>
+                Capital Tours es una empresa líder en el sector de servicios de
+                suscripción de vehículos para taxi en Huancayo - Perú
+              </p>
+              <h5>Nuestra Misión</h5>
+              <p>
+                Nuestra misión en Capital Tours es ofrecer a nuestros clientes
+                conductores de taxi una alternativa innovadora y rentable.
+              </p>
+              <h5>UBICANOS</h5>
+              <p>
+                Dirección: Av. Mariscal Castilla & Av. Gral. Cordova Huancayo,
+                Perú
+                <br />
+                Teléfono: 914354014
+                <br />
+                Horario de Atención: 8:30 am - 7:00 pm{" "}
+              </p>
+            </div>
+          </div>
         </div>
       </section>
       <Footer />
